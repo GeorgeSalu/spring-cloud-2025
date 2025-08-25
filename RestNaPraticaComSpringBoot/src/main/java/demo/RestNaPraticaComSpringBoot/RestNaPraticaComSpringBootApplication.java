82192943e6451ead66cee3868250d141fb60807a
George Salu <george.salu10@gmail.com>
2025-08-25 13:12:52 -0300

    criando projeto RestNaPraticaComSpringBoot

---
+++ RestNaPraticaComSpringBoot/src/main/java/demo/RestNaPraticaComSpringBoot/RestNaPraticaComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package demo.RestNaPraticaComSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNaPraticaComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNaPraticaComSpringBootApplication.class, args);
+	}
+
+}
---
+++ RestNaPraticaComSpringBoot/src/test/java/demo/RestNaPraticaComSpringBoot/RestNaPraticaComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.RestNaPraticaComSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNaPraticaComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
