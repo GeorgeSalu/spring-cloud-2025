33522a2050b79a5ed0733fcf981cf12bd41288e0
George Salu <george.salu10@gmail.com>
2025-08-25 13:19:57 -0300

    RestNaPraticaComSpringBoot - renomeando a classe entripoint da app

--- RestNaPraticaComSpringBoot/src/main/java/demo/RestNaPraticaComSpringBoot/Startup.java
+++ RestNaPraticaComSpringBoot/src/main/java/demo/RestNaPraticaComSpringBoot/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestNaPraticaComSpringBootApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestNaPraticaComSpringBootApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
