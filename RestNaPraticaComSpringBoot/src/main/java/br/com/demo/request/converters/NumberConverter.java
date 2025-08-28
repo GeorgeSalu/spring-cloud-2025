07835fa6dcb0fdc5b46f7fe2fa416782552be7bb
George Salu <george.salu10@gmail.com>
2025-08-28 10:54:00 -0300

    RestNaPraticaComSpringBoot - separando responsabilidade SimpleMath

--- RestNaPraticaComSpringBoot/src/main/java/br/com/demo/request/converters/NumberConverter.java
+++ RestNaPraticaComSpringBoot/src/main/java/br/com/demo/request/converters/NumberConverter.java
@@ -4,13 +4,13 @@ import br.com.demo.exception.UnsupportedMathOperationException;
 
 public class NumberConverter {
 
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+    public static Double convertToDouble(String strNumber) throws IllegalArgumentException {
         if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
 
-    private boolean isNumeric(String strNumber) {
+    public static boolean isNumeric(String strNumber) {
         if(strNumber == null || strNumber.isEmpty()) return false;
         String number = strNumber.replace(",", ".");
         return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
