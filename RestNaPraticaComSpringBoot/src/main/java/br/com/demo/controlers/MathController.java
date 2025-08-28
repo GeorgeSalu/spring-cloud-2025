36150a68d5565ea24cd4dfc9c785287ab73cc33c
George Salu <george.salu10@gmail.com>
2025-08-28 10:46:38 -0300

    RestNaPraticaComSpringBoot - separando responsabilidade NumberConverter

--- RestNaPraticaComSpringBoot/src/main/java/br/com/demo/controlers/MathController.java
+++ RestNaPraticaComSpringBoot/src/main/java/br/com/demo/controlers/MathController.java
@@ -51,16 +51,4 @@ public class MathController {
         return Math.sqrt(convertToDouble(number));
     }
 
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
-        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
-        String number = strNumber.replace(",", ".");
-        return Double.parseDouble(number);
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", ".");
-        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
-    }
-
 }
