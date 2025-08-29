4c39920586118bcf7de0115e6a0ec49374c85ac5
George Salu <george.salu10@gmail.com>
2025-08-29 15:00:34 -0300

    rest-na-pratica-com-spring-boot - renomeando exception customizada

--- rest-na-pratica-com-spring-boot/src/main/java/br/com/demo/controlers/MathController.java
+++ rest-na-pratica-com-spring-boot/src/main/java/br/com/demo/controlers/MathController.java
@@ -1,6 +1,6 @@
 package br.com.demo.controlers;
 
-import br.com.demo.exception.UnsupportedMathOperationException;
+import br.com.demo.exception.ResourceNotFoundException;
 import br.com.demo.math.SimpleMath;
 import br.com.demo.request.converters.NumberConverter;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,42 +16,42 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne,@PathVariable("numberTwo") String numberTwo) {
 
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new ResourceNotFoundException("Please set a numeric value");
         return math.sum( NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
     public Double subtraction(@PathVariable("numberOne") String numberOne,@PathVariable("numberTwo") String numberTwo) {
 
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new ResourceNotFoundException("Please set a numeric value");
         return math.subtraction( NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
     public Double multiplication(@PathVariable("numberOne") String numberOne,@PathVariable("numberTwo") String numberTwo) {
 
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new ResourceNotFoundException("Please set a numeric value");
         return math.multiplication( NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping("/division/{numberOne}/{numberTwo}")
     public Double division(@PathVariable("numberOne") String numberOne,@PathVariable("numberTwo") String numberTwo) {
 
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new ResourceNotFoundException("Please set a numeric value");
         return math.division( NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping("/mean/{numberOne}/{numberTwo}")
     public Double mean(@PathVariable("numberOne") String numberOne,@PathVariable("numberTwo") String numberTwo) {
 
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberOne)) throw new ResourceNotFoundException("Please set a numeric value");
         return math.mean(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping("/squareroot/{number}")
     public Double squareRoot(@PathVariable("number") String number) {
 
-        if (!NumberConverter.isNumeric(number)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if (!NumberConverter.isNumeric(number)) throw new ResourceNotFoundException("Please set a numeric value");
         return math.squareRoot(NumberConverter.convertToDouble(number));
     }
 
--- rest-na-pratica-com-spring-boot/src/main/java/br/com/demo/request/converters/NumberConverter.java
+++ rest-na-pratica-com-spring-boot/src/main/java/br/com/demo/request/converters/NumberConverter.java
@@ -1,11 +1,11 @@
 package br.com.demo.request.converters;
 
-import br.com.demo.exception.UnsupportedMathOperationException;
+import br.com.demo.exception.ResourceNotFoundException;
 
 public class NumberConverter {
 
     public static Double convertToDouble(String strNumber) throws IllegalArgumentException {
-        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
+        if(strNumber == null || strNumber.isEmpty()) throw new ResourceNotFoundException("Please set a numeric value");
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
