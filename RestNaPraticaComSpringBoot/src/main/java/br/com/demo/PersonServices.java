5d71a42605a47a998255639d9da89d846feb9444
George Salu <george.salu10@gmail.com>
2025-08-29 09:41:08 -0300

    RestNaPraticaComSpringBoot - injecao de dependencia com @Service e @Autowired

--- RestNaPraticaComSpringBoot/src/main/java/br/com/demo/PersonServices.java
+++ RestNaPraticaComSpringBoot/src/main/java/br/com/demo/PersonServices.java
@@ -1,12 +1,27 @@
 package br.com.demo;
 
+import br.com.demo.model.Person;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id){
+        logger.info("finding one person");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Rodrigo");
+        person.setAddress("Uberlandia");
+        person.setGender("Male");
+        return person;
+    }
 
 }
