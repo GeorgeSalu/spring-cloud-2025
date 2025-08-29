5a7bb7d84bb126a5c52a1654729b028c0fdeb079
George Salu <george.salu10@gmail.com>
2025-08-29 10:01:52 -0300

    RestNaPraticaComSpringBoot - retornando lista de objetos em json

--- RestNaPraticaComSpringBoot/src/main/java/br/com/demo/PersonServices.java
+++ RestNaPraticaComSpringBoot/src/main/java/br/com/demo/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.demo;
 import br.com.demo.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,19 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        return new Person();
+    }
 
     public Person findById(String id){
         logger.info("finding one person");
