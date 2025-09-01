3c2089e2ccf828c7bf4d240373a388e5f15f1afc
George Salu <george.salu10@gmail.com>
2025-09-01 11:40:48 -0300

    greeting-service - adicionando endpoint greeting

--- greeting-service/src/main/java/br/com/demo/controller/GreetingController.java
+++ greeting-service/src/main/java/br/com/demo/controller/GreetingController.java
@@ -1,7 +1,7 @@
 package br.com.demo.controller;
 
 import br.com.demo.model.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
