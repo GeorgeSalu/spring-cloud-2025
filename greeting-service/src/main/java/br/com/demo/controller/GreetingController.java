6f1b19a9188cda2f59b0c5cb0839f961c3a68cbe
George Salu <george.salu10@gmail.com>
2025-09-01 11:54:35 -0300

    greeting-service - recuperando os valores da configuração

--- greeting-service/src/main/java/br/com/demo/controller/GreetingController.java
+++ greeting-service/src/main/java/br/com/demo/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package br.com.demo.controller;
 
+import br.com.demo.config.GreetingConfiguration;
 import br.com.demo.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,18 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "%s, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private GreetingConfiguration configuration;
+
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
+
+        if (name == null) name = configuration.defaultValue();
+
+        return new Greeting(counter.incrementAndGet(), String.format(configuration.greeting(), name));
     }
 
 }
