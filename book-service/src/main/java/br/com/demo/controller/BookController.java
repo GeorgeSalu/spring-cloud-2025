ba0826fcc1ed7aa652e9133e6c29118fe54546e0
George Salu <george.salu10@gmail.com>
2025-09-04 14:51:18 -0300

    book-service - consumindo o exchange service no book microservice

--- book-service/src/main/java/br/com/demo/controller/BookController.java
+++ book-service/src/main/java/br/com/demo/controller/BookController.java
@@ -1,5 +1,6 @@
 package br.com.demo.controller;
 
+import br.com.demo.dto.Exchange;
 import br.com.demo.enviroment.InstanceInformationService;
 import br.com.demo.model.Book;
 import br.com.demo.repository.BookRepository;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -26,7 +30,16 @@ public class BookController {
 
         var book = repository.findById(id).orElseThrow();
 
+        HashMap<String,String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate().getForEntity("http://localhost:8000/exchange-service/{amount}/{from}/{to}", Exchange.class, params);
+        Exchange exchange = response.getBody();
+
         book.setEnvironment(port);
+        book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
 
         return book;
