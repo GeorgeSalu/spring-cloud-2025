5ce338b2f8bf253c9edc9b68b65717d6521c1f48
George Salu <george.salu10@gmail.com>
2025-09-04 11:18:16 -0300

    exchange-service - removendo os mocks dos controllers

--- exchange-service/src/main/java/br/com/demo/controller/ExchangeController.java
+++ exchange-service/src/main/java/br/com/demo/controller/ExchangeController.java
@@ -2,6 +2,7 @@ package br.com.demo.controller;
 
 import br.com.demo.environment.InstanceInformationService;
 import br.com.demo.model.Exchange;
+import br.com.demo.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,22 @@ public class ExchangeController {
     @Autowired
     private InstanceInformationService informationService;
 
+    @Autowired
+    private ExchangeRepository exchangeRepository;
+
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount,@PathVariable("from") String from,@PathVariable("to") String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT "+informationService.retrieveServerPort());
+
+        Exchange exchange = exchangeRepository.findByFromAndTo(from, to);
+
+        if (exchange == null) throw new RuntimeException("Currency Unsupported");
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        exchange.setConversionFactor(convertedValue);
+        exchange.setEnvironment("PORT "+informationService.retrieveServerPort());
+
+        return exchange;
     }
 
 }
