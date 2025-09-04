ab6e118c2661fe875acfd9aa8e33aa8b72a30246
George Salu <george.salu10@gmail.com>
2025-09-04 09:15:07 -0300

    exchange-service - recuperando a porta da instancia em execução

---
+++ exchange-service/src/main/java/br/com/demo/environment/InstanceInformantionService.java
@@ -0,0 +1,20 @@
+package br.com.demo.environment;
+
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstanceInformantionService implements ApplicationListener<WebServerInitializedEvent> {
+
+    private String port;
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        this.port = String.valueOf(event.getWebServer().getPort());
+    }
+
+    public String retrieveServerPort() {
+        return port;
+    }
+}
