6e300ed1c00989238a38d5fd602d22638d4abd06
George Salu <george.salu10@gmail.com>
2025-09-08 14:59:14 -0300

    api-gateway - configurando o SpringDoc OpenAPI ao usar api gateway

--- api-gateway/src/main/java/br/com/demo/config/OpenAPIConfiguration.java
+++ api-gateway/src/main/java/br/com/demo/config/OpenAPIConfiguration.java
@@ -1,9 +1,16 @@
 package br.com.demo.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfiguration {
@@ -11,6 +18,32 @@ public class OpenAPIConfiguration {
     @Bean
     public SwaggerUiConfigParameters swaggerUiConfigParameters(SwaggerUiConfigProperties properties) {
         return new SwaggerUiConfigParameters(properties);
+    }
+
+    @Bean
+    @Lazy(value = false)
+    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters condif, RouteDefinitionLocator locator) {
+
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        List<GroupedOpenApi> groups = new ArrayList<>();
+
+        if (definitions != null) {
+            definitions.stream()
+                    .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+                    .forEach(
+                            routeDefinition -> {
+                                String name = routeDefinition.getId();
+                                condif.addGroup(name);
+                                groups.add(GroupedOpenApi.builder()
+                                                .group(name)
+                                                .pathsToMatch("/"+name+"/**")
+                                        .build());
+                            }
+                    );
+
+        }
+
+        return groups;
     }
 
 }
