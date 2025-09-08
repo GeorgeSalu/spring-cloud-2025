23dc440b44761fe6cce4a7ea1479c90309301b14
George Salu <george.salu10@gmail.com>
2025-09-08 10:09:27 -0300

    book-service - configurando o swagger openApi no book-service

---
+++ book-service/src/main/java/br/com/demo/config/OpenApiConfiguration.java
@@ -0,0 +1,30 @@
+package br.com.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Book Microservice Api", version = "V1", description = "Documentation of book microservice api")
+)
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI cusOpenApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Book Microservice Api")
+                        .version("V1")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("http://localhost:8100/book-service")
+                        )
+                );
+    }
+
+}
