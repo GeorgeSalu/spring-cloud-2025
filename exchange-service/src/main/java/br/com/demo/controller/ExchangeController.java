01884f458e4c7b5183083fdd3dfdef7b89a1dc7a
George Salu <george.salu10@gmail.com>
2025-09-08 10:59:09 -0300

    exchange-service - documentando com swagger o exchange-service

--- exchange-service/src/main/java/br/com/demo/controller/ExchangeController.java
+++ exchange-service/src/main/java/br/com/demo/controller/ExchangeController.java
@@ -3,6 +3,8 @@ package br.com.demo.controller;
 import br.com.demo.environment.InstanceInformationService;
 import br.com.demo.model.Exchange;
 import br.com.demo.repository.ExchangeRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@Tag(name = "Exchange Endpoint")
 @RestController
 @RequestMapping("exchange-service")
 public class ExchangeController {
@@ -22,6 +25,7 @@ public class ExchangeController {
     @Autowired
     private ExchangeRepository repository;
 
+    @Operation(summary = "Get an exchange from amount of currency")
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount,@PathVariable("from") String from,@PathVariable("to") String to) {
 
